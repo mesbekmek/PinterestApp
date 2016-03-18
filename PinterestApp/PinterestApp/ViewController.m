a6d23468f8fe9e0c209a24af1b2a38b647d322b9
Mesfin Bekele Mekonnen <mesbekmek@gmail.com>
2016-03-18 01:47:52 -0400

    Trying to add images

--- PinterestApp/PinterestApp/ViewController.m
+++ PinterestApp/PinterestApp/ViewController.m
@@ -11,7 +11,8 @@
 #import <PinterestSDK/PinterestSDK.h>
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIImageView *likedImageView;
+@property (nonatomic) NSDictionary *likesDictionary;
 @end
 
 @implementation ViewController
@@ -36,5 +37,46 @@
         NSLog(@"authentication failed: %@", error);
     }];
 }
+
+- (IBAction)getLikesButtonTapped:(UIButton *)sender {
+
+    [[PDKClient sharedInstance] getPath:@"/v1/me/likes/" parameters:nil withSuccess:^(PDKResponseObject *responseObject) {
+        
+        NSLog(@"Response Object: %@",responseObject.parsedJSONDictionary);
+        self.likesDictionary = responseObject.parsedJSONDictionary;
+        [self updateLikeImageView];
+
+        
+    } andFailure:^(NSError *error) {
+        NSLog(@"Error fetching: %@",error.localizedDescription);
+    }];
+}
+
+- (void)updateLikeImageView {
+    
+    NSString *link = [self linkFromDictionary:self.likesDictionary];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+       
+        NSURL *url = [NSURL URLWithString:link];
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        UIImage *image = [UIImage imageWithData:data];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            
+            self.likedImageView.image = image;
+            
+        });
+        
+    });
+}
+
+- (NSString *)linkFromDictionary:(NSDictionary *)dict {
+    NSArray *data = dict[@"data"];
+    NSDictionary *dictFromData = data[0];
+    NSString *link = dictFromData[@"url"];
+    
+    return link;
+}
+
 
 @end
